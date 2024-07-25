0bd42b51a52a06629913c7996f47021471665c4f
Gavin <gnation15@gmail.com>
2024-07-24 22:13:20 -0500

    Added the basics of an observer pattern with the messaging system, including the IMessageHandler interface, the MessageConstants enum, the GameMessage class and the MessageDispatcher singleton.

---
+++ Assets/Scripts/Interfaces/IMessageHandler.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/* This allows us to implement the observer pattern and decouple our GameObjects somewhat.
+ * All/most of the manager objects should implement this interface so they can handle messages from the MessageDispatcher. 
+ * Manager objects should add themselves as a listener to the MessageDispatcher in their start method. */
+
+public interface IMessageHandler
+{
+    void HandleMessage(GameMessage message);
+}
